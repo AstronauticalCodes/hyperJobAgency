0d340dfd24e025ae83627b6e4b35d1d181f2892e
AstronauticalCodes <hyperskillabhinav@gmail.com>
2024-12-05 11:08:13 +0530

    Changed homepage view to project DIR from resume DIR.

--- HyperJob Agency/task/hyperjob/views.py
+++ HyperJob Agency/task/hyperjob/views.py
@@ -1,7 +1,11 @@
-<h2>Welcome to HyperJob!</h2>
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('''<h2>Welcome to HyperJob!</h2>
 <p><a href="/login">Login</a></p>
 <p><a href="/logout">Logout</a></p>
 <p></p><a href="/signup">Signup</a></p>
 <p><a href="/vacancies">Vacancies</a></p>
 <p><a href="/resumes">Resume</a></p>
-<p><a href="/home">Personal Profile</a></p>
+<p><a href="/home">Personal Profile</a></p>''')
